f98ab7545901c067f252c4da1753e1cfc2517772
Kunal <kunalsonawane730@gmail.com>
2022-12-01 00:30:36 +0530

    adding test cases for user first name regex pattern

---
+++ src/test/java/com/userregistration/regexpattern/UserInputValidationTest.java
@@ -0,0 +1,19 @@
+package com.userregistration.regexpattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserInputValidationTest {
+	@Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidFirstName("Dipali");
+        Assert.assertEquals(true, result);
+    }
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidFirstName("Pr");
+        Assert.assertEquals(false, result);
+    }
+}
