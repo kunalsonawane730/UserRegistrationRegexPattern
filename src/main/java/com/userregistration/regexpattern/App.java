cc81ac72e97dd152726488f7c8b7ac294f3e7221
Kunal <kunalsonawane730@gmail.com>
2022-11-30 23:58:44 +0530

    adding welcome message for User Registration Problems

---
+++ src/main/java/com/userregistration/regexpattern/App.java
@@ -0,0 +1,9 @@
+package com.userregistration.regexpattern;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To User Registration Programs" );
+    }
+}
