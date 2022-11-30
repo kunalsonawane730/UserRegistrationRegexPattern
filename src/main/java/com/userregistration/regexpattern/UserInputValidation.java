5967e7b27119f03d14c076c53d204a2d64e1bd86
Kunal <kunalsonawane730@gmail.com>
2022-12-01 00:39:10 +0530

    adding program for user last name regex pattern

--- src/main/java/com/userregistration/regexpattern/UserInputValidation.java
+++ src/main/java/com/userregistration/regexpattern/UserInputValidation.java
@@ -15,4 +15,15 @@ public class UserInputValidation {
 
 	        return matcher.matches();
 	    }
+	    
+	 // Creating isValidLastName method to validate the last name given by user using regex
+	    public static boolean isValidLastName(String lastName) {
+	        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
+
+	        Pattern pattern = Pattern.compile(firstNameRegex);
+
+	        Matcher matcher = pattern.matcher(lastName);
+
+	        return matcher.matches();
+}
 }
