77137d08b702c8c5a91eb5a2d8d106eb8f657b96
Kunal <kunalsonawane730@gmail.com>
2022-12-01 01:16:30 +0530

    adding test cases for user email regex pattern

--- src/test/java/com/userregistration/regexpattern/UserInputValidationTest.java
+++ src/test/java/com/userregistration/regexpattern/UserInputValidationTest.java
@@ -28,5 +28,19 @@ public class UserInputValidationTest {
         UserInputValidation userInputValidation = new UserInputValidation();
         boolean result = userInputValidation.isValidLastName("So");
         Assert.assertEquals(false, result);
-}
+    }
+    @Test
+    public void givenEmail_WhenValid_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidEmail("kunalsonawane730@gmail.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenNotValid_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidEmail("Kunalsonawane..675.gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
 }
