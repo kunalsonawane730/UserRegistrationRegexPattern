00d2d13fb3403ea8b642992c53f7ca36a67600a0
Kunal <kunalsonawane730@gmail.com>
2022-12-12 00:34:40 +0530

    Add: User Registration various regex pattern by using Lambda Expression

--- src/main/java/com/userregistration/regexpattern/UserInputValidation.java
+++ src/main/java/com/userregistration/regexpattern/UserInputValidation.java
@@ -10,95 +10,43 @@ import exceptions.InvalidMobileNumberException;
 import exceptions.InvalidPasswordException;
 
 public class UserInputValidation  {
-	boolean result = false;
-
-    // Method isValidFirstName to validate the first name given by user using regex
-    public boolean isValidFirstName(String firstName) throws InvalidFirstNameException {
-        try {
-
-             // Declaring regex pattern to check the firstName
-            String firstNameRegex = "^[A-Z]{1}[a-z]{2,}$";
-
-             // Compiling the regex
-            Pattern pattern = Pattern.compile(firstNameRegex);
-
-             // Checking fot the pattern match
-
-            Matcher matcher = pattern.matcher(firstName);
-
-            result = matcher.matches();
-            if (!result) {
-                throw new InvalidFirstNameException("Please enter valid input.");
-            }
-        } catch (NullPointerException npe) {
-            throw new InvalidFirstNameException("Given first name is null. Please enter valid input.");
-        }
-        return result;
-        }
- // method isValidLastName to validate the last name given by user using regex
-    public boolean isValidLastName(String lastName) throws InvalidLastNameException {
-        try {
-
-            String lastNameRegex = "^[A-Z]{1}[a-z]{2,}$";
-            Pattern pattern = Pattern.compile(lastNameRegex);
-            Matcher matcher = pattern.matcher(lastName);
-            result = matcher.matches();
-            if (!result) {
-                throw new InvalidLastNameException("Please enter valid input.");
-            }
-        } catch (NullPointerException npe) {
-            throw new InvalidLastNameException("Given last name is null. Please enter valid input.");
-        }
-        return result;
+    @FunctionalInterface
+    public interface Validate {
+        // Declaring an Abstract method
+        boolean validate(String input);
     }
 
-    //method isValidEmail to validate the email given by user using regex
-    public boolean isValidEmail(String email) throws InvalidEmailException {
-        try {
-            String emailRegex = "^[a-zA-Z-9]+([._+-]?[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2})?$";
-            Pattern pattern = Pattern.compile(emailRegex);
-            Matcher matcher = pattern.matcher(email);
-            result = matcher.matches();
-            if (!result) {
-                throw new InvalidEmailException("Please enter valid input.");
-            }
-        } catch (NullPointerException npe) {
-            throw new InvalidEmailException("Given email is null. Please enter valid input.");
-        }
-        return result;
+    //method validateFirstName to validate first name using regex using lambda function
+    //method return - Validate type
+
+    public static Validate validateFirstName() {
+        return firstName -> firstName.matches("^[A-Z]{1}[a-z]{2,}$");
     }
 
- // method isValidMobileNumber to validate the mobile format given by user using regex
-    public boolean isValidMobileNumber(String mobileNumber) throws InvalidMobileNumberException {
-        try {
-            String mobileNumberRegex = "^[0-9]{2}[\\s][0-9]{10}$";
-            Pattern pattern = Pattern.compile(mobileNumberRegex);
-            Matcher matcher = pattern.matcher(mobileNumber);
-            result = matcher.matches();
-            if (!result) {
-                throw new InvalidMobileNumberException("Please enter valid input.");
-            }
-        } catch (NullPointerException npe) {
-            throw new InvalidMobileNumberException("Given email is null. Please enter valid input.");
-        }
-        return result;
+    // method validateLastName to validate last name using regex using lambda function
+    // Validate type
+    public static Validate validateLastName() {
+        return lastName -> lastName.matches("^[A-Z]{1}[a-z]{2,}$");
     }
 
-    //method isValidPassword to validate the password given by user using regex
-    public boolean isValidPassword(String password) throws InvalidPasswordException {
-        try {
-            String passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*" +
-                    "[\\@\\#\\^\\!\\$\\%\\&\\?][a-zA-Z0-9]*$";
+    //Method validateEmail to validate email using regex using lambda function
+    //return - Validate type
+    public static Validate validateEmail() {
+        return email -> email.matches("^[a-zA-Z-9]+([._+-]?[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2})?$");
+    }
 
-            Pattern pattern = Pattern.compile(passwordRegex);
-            Matcher matcher = pattern.matcher(password);
-            result = matcher.matches();
-            if (!result) {
-                throw new InvalidPasswordException("Please enter valid input.");
-            }
-        } catch (NullPointerException npe) {
-            throw new InvalidPasswordException("Given email is null. Please enter valid input.");
-        }
-        return result;
+    //method validateMobileNumber to validate mobile number using regex using lambda function
+    //return - Validate type
+    public static Validate validateMobileNumber() {
+        return mobileNumber -> mobileNumber.matches("^[0-9]{2}[\\s][0-9]{10}$");
+    }
+
+
+    // method validatePassword to validate password using regex using lambda function
+    //return - Validate type
+
+    public static Validate validatePassword() {
+        return password -> password.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*" +
+                "[\\@\\#\\^\\!\\$\\%\\&\\?][a-zA-Z0-9]*$");
     }
 }
--- src/main/java/com/userregistration/regexpattern/UserRegistration.java
+++ src/main/java/com/userregistration/regexpattern/UserRegistration.java
@@ -1,27 +1,45 @@
 package com.userregistration.regexpattern;
 
-/*import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-/*public class UserRegistration {
+public class UserRegistration {
+    public static void main(String[] args) throws IOException {
 
-	/*public static void main(String[] args) {
-		UserInputValidation userInputValidation = new UserInputValidation();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first name: ");
-        String firstName = scanner.next();
-        System.out.println(userInputValidation.isValidFirstName(firstName) ? "Valid" : "In Valid");
-        System.out.println("Enter last name: ");
-        String lastName = scanner.next();
-        System.out.println(userInputValidation.isValidLastName(lastName) ? "Valid" : "In Valid");
-        System.out.println("Enter email: ");
-        String email = scanner.next();
-        System.out.println(userInputValidation.isValidEmail(email) ? "Valid" : "In Valid");
-        System.out.println("Enter mobile number: ");
-        String mobileNumber = scanner.next();
-        System.out.println(userInputValidation.isValidMobileNumber(mobileNumber) ? "Valid" : "In Valid");
-        System.out.println("Enetr password: ");
-        String password = scanner.next();
-        System.out.println(userInputValidation.isValidPassword(password) ? "Valid" : "In Valid");
-        scanner.close();
+        UserInputValidation userInputValidationUtil = new UserInputValidation();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("******* FIRSTNAME VALIDATION *******");
+            System.out.println("Please Enter first name: ");
+            String firstName = bufferedReader.readLine();
+            System.out.println(UserInputValidation.validateFirstName().validate(firstName) ? "Valid" : "InValid");
+            System.out.println("******* LASTNAME VALIDATION *******");
+            System.out.println("Please Enter last name: ");
+            String lastName = bufferedReader.readLine();
+            System.out.println(UserInputValidation.validateLastName().validate(lastName) ? "Valid" : "InValid");
+            System.out.println("******* EMAIL VALIDATION *******");
+            System.out.println("Please Enter email: ");
+            String email = bufferedReader.readLine();
+            System.out.println(UserInputValidation.validateEmail().validate(email) ? "Valid" : "InValid");
+            System.out.println("******* MOBILE FORMAT VALIDATION *******");
+            System.out.println("Please Enter Mobile Number: ");
+            String mobileNumber = bufferedReader.readLine();
+            System.out.println(UserInputValidation.validateMobileNumber().validate(mobileNumber) ? "Valid" : "InValid");
+            System.out.println("******* PASSWORD VALIDATION *******");
+            System.out.println("Please Enter Password: ");
+            String password = bufferedReader.readLine();
+            System.out.println(UserInputValidation.validatePassword().validate(password) ? "Valid" : "InValid");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            bufferedReader.close();
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 	}
-}*/
+}
